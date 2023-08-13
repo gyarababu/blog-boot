2fd6d368bd5210941974f0440e0dc07c420c2744
Gyarababu <gyarababu9@gmail.com>
2023-08-13 11:31:45 +0530

    Getting all comments for a single post

--- src/main/java/com/blog/boot/payload/PostDto.java
+++ src/main/java/com/blog/boot/payload/PostDto.java
@@ -2,10 +2,15 @@ package com.blog.boot.payload;
 
 import lombok.*;
 
+import java.util.Set;
+
 @Data
 public class PostDto {
     private Long id;
     private String title;
     private String description;
     private String content;
+
+    // getting all comments for post
+    private Set<CommentDto> comments;
 }
