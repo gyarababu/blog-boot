35b8a7087f53eab486dae6ec2214fc4cd4dd9f28
Gyarababu <gyarababu9@gmail.com>
2023-08-15 10:38:42 +0530

    Created Repositories for Users and Roles

---
+++ src/main/java/com/blog/boot/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.blog.boot.repository;
+
+import com.blog.boot.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String userName);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUserNameOrEmail(String userName, String email);
+    boolean existsByUserName(String userName);
+    boolean existsByEmail(String email);
+}
