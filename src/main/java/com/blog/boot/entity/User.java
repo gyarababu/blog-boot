a34ee798afe954e5f3f68a0f46189f865580b386
Gyarababu <gyarababu9@gmail.com>
2023-08-15 09:50:28 +0530

    Created Users and Roles Entities and Created another table using these as Users_Roles

---
+++ src/main/java/com/blog/boot/entity/User.java
@@ -0,0 +1,44 @@
+package com.blog.boot.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    private String email;
+
+    // FetchType.EAGER: related entities or collections will be loaded immediately along with the owning entity,
+    // whenever that entity is fetched from the database
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    // creating another table since we use @ManyToMany
+    @JoinTable(name = "users_roles",
+    joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles;
+}
