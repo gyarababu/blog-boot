f8f0f33ca701d3382e69b38ea7a8ecc4b2796da3
Gyarababu <gyarababu9@gmail.com>
2023-08-26 20:27:56 +0530

    created metadata to be stored in database at application startup

--- src/main/java/com/blog/boot/BlogBootApplication.java
+++ src/main/java/com/blog/boot/BlogBootApplication.java
@@ -1,9 +1,13 @@
 package com.blog.boot;
 
+import com.blog.boot.entity.Role;
+import com.blog.boot.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,7 +24,7 @@ import org.springframework.context.annotation.Bean;
 				)
 		)
 )
-public class BlogBootApplication {
+public class BlogBootApplication implements CommandLineRunner {
 
 	// add ModelMapper bean
 	@Bean
@@ -32,4 +36,22 @@ public class BlogBootApplication {
 		SpringApplication.run(BlogBootApplication.class, args);
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Check if roles already exist in the database
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			Role adminRole = new Role();
+			adminRole.setName("ROLE_ADMIN");
+			roleRepository.save(adminRole);
+		}
+
+		if (roleRepository.findByName("ROLE_USER") == null) {
+			Role userRole = new Role();
+			userRole.setName("ROLE_USER");
+			roleRepository.save(userRole);
+		}
+	}
 }
